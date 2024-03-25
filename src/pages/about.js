0ef87d04450ef26b0827660be043912cda9cc9c1
djikicgoran1966 <djikicgoran1966@gmail.com>
2024-03-25 14:30:47 +0100

    initial comit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const styleapp={
+    fontWeight:"bolder",
+    fontSize:"2rem"
+}
+
+const Aboutpage = () => {
+  return (
+    <div style={styleapp}>About page</div>
+  )
+}
+
+export default Aboutpage
+
+export const Head=()=><title>About page</title>
